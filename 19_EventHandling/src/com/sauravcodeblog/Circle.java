6ab366dddec5d374883ce78dba6325f50b53bc3e
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-07-23 02:59:50 +0530

    .
    .

---
+++ 19_EventHandling/src/com/sauravcodeblog/Circle.java
@@ -0,0 +1,45 @@
+package com.sauravcodeblog;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle implements Shape{
+	@Autowired
+	private MessageSource messageSource;
+	
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	public Point getCenter() {
+		return center;
+	}
+	
+	@Resource(name="pointB")
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+	
+	private Point center;
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing Circle");
+//		System.out.println("Point is ("+center.getX()+","+center.getY()+")");
+		System.out.println(this.messageSource.getMessage("circle.point", new Object[]{center.getX(), center.getY()}, "myDefault Points", null));
+
+		String mesg = this.messageSource.getMessage("circleClass.example",null,"Default Message here",null);
+		System.out.println(mesg);
+	}
+
+}
